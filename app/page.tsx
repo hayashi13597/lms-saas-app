05b90425e8bc3c2c839470b9ce8bc8e2baf1132a
Phuc Lam <lam-h@dymvietnam.net>
2025-07-16 22:24:58 +0700

    chore: update dependencies and add new packages for form handling and UI components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import CompanionCard from "@/components/CompanionCard";
+import CTA from "@/components/CTA";
+import { recentSessions } from "@/constants";
+import React from "react";
+import CompanionsList from "@/components/CompanionsList";
 
 const Page = () => {
   return (
-    <div>Page</div>
-  )
-}
+    <main>
+      <h1>Popular Companions</h1>
 
-export default Page
+      <section className="home-section">
+        {recentSessions
+          .slice(0, 3)
+          .map(({ id, name, topic, subject, duration, color }) => (
+            <CompanionCard
+              key={id}
+              id={id}
+              name={name}
+              topic={topic}
+              subject={subject}
+              duration={duration}
+              color={color}
+            />
+          ))}
+      </section>
+
+      <section className="home-section">
+        <CompanionsList
+          title="Recently completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
+  );
+};
+
+export default Page;
